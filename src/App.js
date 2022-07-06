7aaf908e1d066afcfa6ff654e8336a286cfdde65
Jesus <jcarranza@joonik.com>
2022-07-05 20:31:39 -0500

    prueba v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import translate from 'translate-google-api';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [textoTraducido, setTexto] = useState('');
+	const handleTraductor = async (e) => {
+		const result = await translate(`I'm fine.`, {
+			tld: 'cn',
+			to: 'vi'
+		});
+		console.log('result', result);
+		setTexto(result);
+		return result;
+	};
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+				<a href='!#' onClick={() => handleTraductor()}>
+					TRADUCIR
+				</a>
+				<div>{textoTraducido || `No hay`}</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
